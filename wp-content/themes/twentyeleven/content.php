8def76656a5fd601b9078fe5f45603cb0d4add20
maoquan <3610@3610.cn>
2019-07-16 09:37:54 +0800

    搬迁到阿里云

--- wp-content/themes/twentyeleven/content.php
+++ wp-content/themes/twentyeleven/content.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The default template for displaying content
+ * Template for displaying content
  *
  * @package WordPress
  * @subpackage Twenty_Eleven
@@ -12,11 +12,11 @@
 		<header class="entry-header">
 			<?php if ( is_sticky() ) : ?>
 				<hgroup>
-					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 					<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
 				</hgroup>
 			<?php else : ?>
-			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 			<?php endif; ?>
 
 			<?php if ( 'post' == get_post_type() ) : ?>
@@ -39,7 +39,14 @@
 		<?php else : ?>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+			<?php
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>',
+					'after'  => '</div>',
+				)
+			);
+?>
 		</div><!-- .entry-content -->
 		<?php endif; ?>
 
@@ -49,11 +56,13 @@
 			<?php
 				/* translators: used between list items, there is a space after the comma */
 				$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
-				if ( $categories_list ):
+			if ( $categories_list ) :
 			?>
 			<span class="cat-links">
-				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
-				$show_sep = true; ?>
+			<?php
+				printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
+			$show_sep = true;
+				?>
 			</span>
 			<?php endif; // End if categories ?>
 			<?php endif; // End if is_object_in_taxonomy( get_post_type(), 'category' ) ?>
@@ -61,13 +70,16 @@
 			<?php
 				/* translators: used between list items, there is a space after the comma */
 				$tags_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
-				if ( $tags_list ):
-				if ( $show_sep ) : ?>
+			if ( $tags_list ) :
+				if ( $show_sep ) :
+				?>
 			<span class="sep"> | </span>
 				<?php endif; // End if $show_sep ?>
 			<span class="tag-links">
-				<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list );
-				$show_sep = true; ?>
+				<?php
+				printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list );
+				$show_sep = true;
+				?>
 			</span>
 			<?php endif; // End if $tags_list ?>
 			<?php endif; // End if is_object_in_taxonomy( get_post_type(), 'post_tag' ) ?>
